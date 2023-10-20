78f08ee605c7a1d7b3899cacbd196895ee42ffce
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-20 22:21:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,83 @@
+#Importing required packages
+import streamlit as st
+import openai
+import promptlayer
 
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+promptlayer.api_key = st.secrets["PROMPTLAYER"]
+#MODEL = "gpt-3"
+#MODEL = "gpt-3.5-turbo"
+#MODEL = "gpt-3.5-turbo-0613"
+#MODEL = "gpt-3.5-turbo-16k"
+MODEL = "gpt-3.5-turbo-16k-0613"
+#MODEL = "gpt-4"
+#MODEL = "gpt-4-0613"
+#MODEL = "gpt-4-32k-0613"
+
+# Swap out your 'import openai'
+openai = promptlayer.openai
+
+st.set_page_config(page_title="Learn Wardley Mapping Bot")
+st.sidebar.title("Learn Wardley Mapping")
+st.sidebar.divider()
+st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
+st.sidebar.markdown("Current Version: 0.2.0")
+st.sidebar.markdown("Using GPT-4 API")
+st.sidebar.divider()
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = MODEL
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    st.session_state.messages.append({
+            "role": "system",
+            "content": f"""
+             Interact with WardleyMapBot, your personal guide to learning and creating Wardley Maps.
+             Discover the power of Wardley Mapping for strategic planning and decision-making by choosing to 'Learn' about the components of a Wardley Map, or 'Vocabulary' and I will provide a list of common terms and their definitions. or 'Create' your own map with step-by-step guidance.
+             If you need assistance, type 'Help' for support. Begin your Wardley Mapping journey now!
+             """
+        })
+    st.session_state.messages.append(   
+        {
+            "role": "user",
+            "content": "Help?"
+        })
+    st.session_state.messages.append(
+        {
+            "role": "assistant",
+            "content": """
+            I'm here to help you learn about and create Wardley Maps. Here are some options for getting started:
+            1. Learn: To learn about the components and concepts of a Wardley Map, type "Learn".
+            2. Vocabulary: To get a list of common Wardley Map terms and their definitions, type "Vocabulary".
+            3. Create: To create your own Wardley Map with step-by-step guidance, type "Create".
+            If you have any specific questions or need clarification on any aspect of Wardley Mapping, feel free to ask.
+            """
+        })
+
+for message in st.session_state.messages:
+    if message["role"] in ["user", "assistant"]:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+            pl_tags=["wardleymapbot"]
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
