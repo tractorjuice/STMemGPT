0780e5d5b7aa6cba27780bdffa455936fbd59182
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-22 17:31:50 +0100

    Update agent.py

--- memgpt/agent.py
+++ memgpt/agent.py
@@ -559,12 +559,9 @@ class AgentAsync(object):
                 self.interface.user_message(user_message)
                 packed_user_message = {'role': 'user', 'content': user_message}
                 input_message_sequence = self.messages + [packed_user_message]
-                #st.session_state.all_new_messages.append(packed_user_message)
-                #input_message_sequence = st.session_state.all_new_messages
                 
             else:
                 input_message_sequence = self.messages
-                #input_message_sequence = st.session_state.all_new_messages
 
             if len(input_message_sequence) > 1 and input_message_sequence[-1]['role'] != 'user':
                 printd(f"WARNING: attempting to run ChatCompletion without user as the last message in the queue")
@@ -628,7 +625,6 @@ class AgentAsync(object):
                 printd(f"last response total_tokens ({current_total_tokens}) < {MESSAGE_SUMMARY_WARNING_TOKENS}")
 
             self.append_to_messages(all_new_messages)
-            st.session_state.all_new_messages.append(all_new_messages)
             return all_new_messages, heartbeat_request, function_failed, active_memory_warning
 
         except Exception as e:
