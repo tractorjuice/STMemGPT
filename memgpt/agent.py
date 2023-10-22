361b41a13d89869998eafd21dd032e1863428a81
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-22 21:20:42 +0100

    Update agent.py

--- memgpt/agent.py
+++ memgpt/agent.py
@@ -620,6 +620,7 @@ class AgentAsync(object):
             active_memory_warning = False
             if current_total_tokens > MESSAGE_SUMMARY_WARNING_TOKENS:
                 printd(f"WARNING: last response total_tokens ({current_total_tokens}) > {MESSAGE_SUMMARY_WARNING_TOKENS}")
+                st.sidebar.write(f"WARNING: last response total_tokens ({current_total_tokens}) > {MESSAGE_SUMMARY_WARNING_TOKENS}")
                 # Only deliver the alert if we haven't already (this period)
                 if not self.agent_alerted_about_memory_pressure:
                     active_memory_warning = True
