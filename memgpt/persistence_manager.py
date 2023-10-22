e18230ff4797cabd95107cb47080cb4a5a29e755
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-22 11:23:42 +0100

    Update persistence_manager.py

--- memgpt/persistence_manager.py
+++ memgpt/persistence_manager.py
@@ -111,7 +111,7 @@ class InMemoryStateManager(PersistenceManager):
         st.session_state.persistence_messages = st.session_state.persistence_messages + added_messages
         self.messages = st.session_state.persistence_messages
 
-         st.sidebar.error(st.session_state.persistence_messages)
+        st.sidebar.error(st.session_state.persistence_messages)
     
         #self.all_messages.extend(added_messages)
         st.session_state.persistence_all_messages.extend(added_messages)
