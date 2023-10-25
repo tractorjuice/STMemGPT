fb4d381f6f2d70e7c31884834a105858f97830e1
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-25 15:50:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -99,12 +99,17 @@ for message in st.session_state.messages:
             st.markdown(message["content"])
 
 # Skip user inputs if there's a memory warning, function execution failed, or the agent asked for control
-if st.session_state.token_warning:
-    user_message = system.get_token_limit_warning()
-if st.session_state.function_failed:
-    user_message = system.get_heartbeat(constants.FUNC_FAILED_HEARTBEAT_MESSAGE)
-if st.session_state.heartbeat_request:
-    user_message = system.get_heartbeat(constants.REQ_HEARTBEAT_MESSAGE)
+
+if st.session_state.token_warning or st.session_state.function_failed or st.session_state.heartbeat_request:
+    # Assign the appropriate value to user_message based on the condition that's true
+    if st.session_state.token_warning:
+        user_message = system.get_token_limit_warning()
+    elif st.session_state.function_failed:
+        user_message = system.get_heartbeat(constants.FUNC_FAILED_HEARTBEAT_MESSAGE)
+    elif st.session_state.heartbeat_request:
+        user_message = system.get_heartbeat(constants.REQ_HEARTBEAT_MESSAGE)
+
+    new_messages, st.session_state.heartbeat_request, st.session_state.function_failed, st.session_state.token_warning = st.session_state.memgpt_agent.step(user_message, first_message=False, skip_verify=True)
 
 if prompt := st.chat_input("How can I help with Wardley Mapping?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
