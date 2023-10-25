b0c9d91327d4222f3c48235179e7830f00f5f34a
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-25 14:45:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -97,13 +97,6 @@ for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-if prompt := st.chat_input("How can I help with Wardley Mapping?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
-    user_message = system.package_user_message(prompt)
-    new_messages, st.session_state.heartbeat_request, st.session_state.function_failed, st.session_state.token_warning = st.session_state.memgpt_agent.step(user_message, first_message=False, skip_verify=True)
-
 # Skip user inputs if there's a memory warning, function execution failed, or the agent asked for control
 if st.session_state.token_warning:
     user_message = system.get_token_limit_warning()
@@ -117,6 +110,13 @@ if st.session_state.heartbeat_request:
     user_message = system.get_heartbeat(constants.REQ_HEARTBEAT_MESSAGE)
     new_messages, heartbeat_request, function_failed, token_warning = st.session_state.memgpt_agent.step(user_message, first_message=False, skip_verify=True)
     st.session_state.heartbeat_request = False
+    
+if prompt := st.chat_input("How can I help with Wardley Mapping?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+    user_message = system.package_user_message(prompt)
+    new_messages, st.session_state.heartbeat_request, st.session_state.function_failed, st.session_state.token_warning = st.session_state.memgpt_agent.step(user_message, first_message=False, skip_verify=True)
 
 st.sidebar.divider()
 st.sidebar.write(f"Heartbeat: {st.session_state.heartbeat_request}")
@@ -127,10 +127,11 @@ st.sidebar.write(f"Msg Total: {st.session_state.messages_total}")
 st.sidebar.divider()
 st.sidebar.write(f"Pers Msg: {st.session_state.persistence_all_messages}")
 
-#for item in new_messages:
-#####    if 'function_call' in item and 'arguments' in item['function_call']:
-#        message_args = json.loads(item['function_call']['arguments'])
-#        if 'message' in message_args:
-#            message = message_args['message']
-#            st.write(message)
-#            st.session_state.messages.append({"role": "assistant", "content": message})
+for item in new_messages:
+    if 'function_call' in item and 'arguments' in item['function_call']:
+        message_args = json.loads(item['function_call']['arguments'])
+        if 'message' in message_args:
+            message = message_args['message']
+            st.write(message)
+            st.session_state.messages.append({"role": "assistant", "content": message})
+            
