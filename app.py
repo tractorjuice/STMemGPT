c5fab2f13df8472538fe6aa1c2d12439ab8ffdfe
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-25 22:43:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def process_assistant_messages(new_messages):
                     st.session_state.messages.append({"role": "assistant", "content": response})
             except json.JSONDecodeError:
                 st.warning("There was an error parsing the message from the assistant.")
-    return(response)
+        return(response)
 
 def process_user_messages(new_messages):
     for item in new_messages:
@@ -98,7 +98,7 @@ def process_user_messages(new_messages):
                     st.session_state.messages.append({"role": "user", "content": response})
             except json.JSONDecodeError:
                 st.warning("There was an error parsing the message from the assistant.")
-    return(response)
+        return(response)
 
 if not st.session_state.memgpt_agent:
     if MODE == "Archive":
