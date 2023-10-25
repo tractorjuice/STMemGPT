70227a6bb665ea7ab513f5c80e2c2d6d2ac1d45c
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-25 22:08:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -93,10 +93,10 @@ if not st.session_state.memgpt_agent:
     memgpt_agent = presets.use_preset('memgpt_chat', MODEL, personas.get_persona_text(PERSONA), humans.get_human_text(HUMAN), interface, persistence_manager)
     st.session_state.memgpt_agent = memgpt_agent
 
-    for message in st.session_state.messages:
-        if message["role"] in ["user", "assistant"]:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+for message in st.session_state.messages:
+    if message["role"] in ["user", "assistant"]:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
             
 if prompt := st.chat_input("How can I help with Wardley Mapping?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
