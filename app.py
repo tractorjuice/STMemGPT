2c112b36a76094b37d63e93f4918e0e2a0201a8c
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-26 01:59:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ st.sidebar.title("Ultimate AI Assistant")
 st.sidebar.title("Wardley Mapping Version")
 st.sidebar.divider()
 st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
-st.sidebar.markdown("Current Version: 1.0.0")
+st.sidebar.markdown("Current Version: 1.3.0")
 st.sidebar.divider()
 
 def clean_and_parse_json(raw_json):
