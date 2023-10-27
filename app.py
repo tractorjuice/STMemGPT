06f73f00bf818526305738a168b8a9d14fad4922
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-27 17:54:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -170,5 +170,5 @@ if st.session_state.heartbeat_request:
         new_messages, st.session_state.heartbeat_request, st.session_state.function_failed, st.session_state.token_warning = st.session_state.memgpt_agent.step(user_message, first_message=False, skip_verify=True)
         response = process_assistant_messages(new_messages)
     st.write("Cleared prompt!")
-    prompt = "Cleared Prompt!"
+    st.session_state.user_input = ""
     #st.rerun()
