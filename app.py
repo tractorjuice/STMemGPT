271ebb30dcaf53765254b1e75e7e5b1c7369a68e
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-27 19:30:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ st.sidebar.title("Ultimate AI Assistant")
 st.sidebar.title("Wardley Mapping Version")
 st.sidebar.divider()
 st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
-st.sidebar.markdown("Current Version: 1.3.3")
+st.sidebar.markdown("Current Version: 1.3.9")
 st.sidebar.divider()
 
 # Check if the user has provided an API key
