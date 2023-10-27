9e556c3dfc8e78f534455ebc9dbd307f4873a93f
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-27 18:46:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -141,8 +141,7 @@ for message in st.session_state.messages:
 if user_openai_api_key:
     st.write(st.session_state.heartbeat_request)
     if st.session_state.heartbeat_request == None or st.session_state.heartbeat_request == "False":
-        prompt = ""
-        prompt = st.chat_input("How can I help with Wardley Mapping?")
+        prompt = st.chat_input(placeholder="How can I help with Wardley Mapping?", key="chat")
         if prompt:
             st.session_state.messages.append({"role": "user", "content": prompt})
             st.write("Running prompt code:")
