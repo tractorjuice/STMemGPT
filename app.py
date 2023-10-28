fc349f8bbd59fa913d9c1231fdc487c20dfbcdf0
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-28 13:09:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ st.sidebar.title("Ultimate AI Assistant (SPR)")
 st.sidebar.title("Wardley Mapping Version")
 st.sidebar.divider()
 st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
-st.sidebar.markdown("Current Version: 1.6.0L")
+st.sidebar.markdown("Current Version: 1.6.0L-SPR")
 
 # Show session state id, used to identifying individual browsers for diagnostics.
 #st.sidebar.write(st.session_state.session_id)
