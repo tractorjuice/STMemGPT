457af3ce335af18c2e6c8c9694358deb2aa14d60
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-28 23:29:23 +0100

    Update agent.py

--- memgpt/agent.py
+++ memgpt/agent.py
@@ -93,8 +93,8 @@ def get_ai_reply_async(
     ):
     """Base call to GPT API w/ functions"""
 
-    st.sidebar.write("[langchain] converting openai to langchain")
-    message_sequence = convert_openai_messages(message_sequence)
+        st.sidebar.write("[langchain] converting openai to langchain")
+        message_sequence = convert_openai_messages(message_sequence)
     
         try:
         response = acreate(
