7ab00fcc0cc51fab4cc24862c88ba297ec13310f
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-29 23:35:24 +0000

    Update agent.py

--- memgpt/agent.py
+++ memgpt/agent.py
@@ -199,22 +199,21 @@ class AgentAsync(object):
             st.session_state["persistence_manager"] = persistence_manager
         st.session_state.persistence_manager = persistence_manager
         self.persistence_manager = st.session_state.persistence_manager
-
-        # Trying to fix Archive not there until first call of update/amend, etc
-
-        st.session_state.agent_messages = initialize_message_sequence(
-            #self.system,
-            st.session_state.agent_system,
-            #self.memory,
-            st.session_state.agent_memory,
-            archival_memory=self.persistence_manager.archival_memory,
-            recall_memory=self.persistence_manager.recall_memory,
-        )
-        self._messages = st.session_state.agent_messages
         
         if persistence_manager_init:
             # creates a new agent object in the database
             self.persistence_manager.init(self)
+                    # Trying to fix Archive not there until first call of update/amend, etc
+
+            st.session_state.agent_messages = initialize_message_sequence(
+                #self.system,
+                st.session_state.agent_system,
+                #self.memory,
+                st.session_state.agent_memory,
+                archival_memory=self.persistence_manager.archival_memory,
+                recall_memory=self.persistence_manager.recall_memory,
+            )
+            self._messages = st.session_state.agent_messages
 
         # State needed for heartbeat pausing
         if "pause_heartbeats_start" not in st.session_state:
